6a0618b29f03d3a7e0704c3905060fbe6aa36939
lucaszeng <lucaszeng99@gmail.com>
2018-03-21 20:45:29 -0600

    finish to guide progress.

--- app.js
+++ app.js
@@ -21,32 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.get('/onlineusers', function(req, res) {
-  res.send(io.sockets.adapter.rooms);
-})
 
 
+// the code below prevents furthur routing configuration outside this file.
+// all the routing must be done before the error routing, or a 404 will be responded.
 
-
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
